da902cbc14ee6cf75ce527c227779e7cd60754c8
Oleksandr <lysakova@yahoo.com>
2022-03-22 05:32:49 +0100

    initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Header.css'
+import {Route, Routes, Link} from 'react-router-dom'
+import Home from '../home/Home'
+import Gallery from '../gallery/Gallery'
+import Market from '../market/Market'
+import Wallet from '../wallet/Wallet'
+import Mint from '../mint/Mint'
+
+function Header() {
+  return (
+    <div>
+        <nav>
+        <Link to="/">Home</Link>
+        <Link to="/gallery">Gallery</Link>
+        <Link to="/market">Market</Link>
+        <Link to="/wallet">My wallet</Link>
+        <Link to="/mint">Mint</Link>
+        </nav>
+        <Routes>
+            <Route exact path='/' caseSensitive={false} element={<Home/>}/>
+            <Route  path='/mint' caseSensitive={false} element={<Mint/>}/>
+            <Route  path='/gallery' caseSensitive={false} element={<Gallery/>}/>
+            <Route  path='/wallet' caseSensitive={false} element={<Wallet/>}/>
+            <Route  path='/market' caseSensitive={false} element={<Market/>}/>
+        </Routes>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/punk/Punk.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Punk() {
+  return (
+    <div>Punk</div>
+  )
+}
+
+export default Punk
