d7aa55c14fd7ea787d56af313898ddb581c70f52
Oleksandr <lysakova@yahoo.com>
2023-04-09 18:15:40 +0200

    change soon to mint button

--- src/components/pages/mint/Mint.jsx
+++ src/components/pages/mint/Mint.jsx
@@ -1,53 +1,56 @@
-import React, { useState } from 'react'
-import './Mint.css'
-import abi from '../../../web3/abi.json'
-import contractAddress from '../../../web3/contract'
-import { ethers } from 'ethers'
-import Swal from 'sweetalert2'
-import * as utils from '../../../web3/utils'
+import React, { useState } from 'react';
+import './Mint.css';
+import abi from '../../../web3/abi.json';
+import contractAddress from '../../../web3/contract';
+import { ethers } from 'ethers';
+import Swal from 'sweetalert2';
+import * as utils from '../../../web3/utils';
 
-
-const ownerAddress = '0xF70DD4EEaC30aAD82e7A747c06bd44ff438B8813'
-const MAX_MINT = 25
-const MINT_PRICE = '50'
+const ownerAddress = '0xF70DD4EEaC30aAD82e7A747c06bd44ff438B8813';
+const MAX_MINT = 25;
+const MINT_PRICE = '50';
 
 function Mint({ walletConnectedDetails, connect }) {
-  const [mintQuantity, setMintQuantity] = useState(1)
+  const [mintQuantity, setMintQuantity] = useState(1);
 
-  const { browserWeb3Provider, address, connected } = walletConnectedDetails
+  const { browserWeb3Provider, address, connected } = walletConnectedDetails;
 
   const handleMintSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (!connected) {
-      Swal.fire('Connect your wallet first please!')
+      Swal.fire('Connect your wallet first please!');
     }
     if (connected) {
       try {
-        const signer = browserWeb3Provider.getSigner()
-        const nftContract = new ethers.Contract(contractAddress, abi, signer)
+        const signer = browserWeb3Provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, abi, signer);
         let nftTxn = await nftContract.mint(address, mintQuantity, {
-          value: ethers.utils.parseEther((MINT_PRICE * mintQuantity).toString()),
-        })
-        await nftTxn.wait()
+          value: ethers.utils.parseEther(
+            (MINT_PRICE * mintQuantity).toString()
+          ),
+        });
+        await nftTxn.wait();
         Swal.fire({
           position: 'top-end',
           icon: 'success',
-          title: `Succes, you minted ${mintQuantity} Cpunk${mintQuantity === 1 ? '' : 's'}!`,
+          title: `Succes, you minted ${mintQuantity} Cpunk${
+            mintQuantity === 1 ? '' : 's'
+          }!`,
           showConfirmButton: false,
           timer: 2000,
-        })
+        });
       } catch (err) {
-        console.log(err.message)
+        console.log(err.message);
         Swal.fire({
           position: 'top-end',
           icon: 'error',
           title: `${err.message}`,
           showConfirmButton: false,
           timer: 2000,
-        })
+        });
       }
     }
-  }
+  };
   /**Send from */
   // const sendPunk = async (from, to, id) => {
   //   try {
@@ -64,13 +67,13 @@ function Mint({ walletConnectedDetails, connect }) {
   //   e.preventDefault()
   //   sendPunk(ownerAddress, '0xc278ec3c330bdda5980bf12785b20435cd9380dd',2793)
   // }
- /**Send from */
+  /**Send from */
   const onChangeMintQuantity = (e) => {
-    const { value } = e.target
+    const { value } = e.target;
     if (value > 0 && value <= MAX_MINT) {
-      setMintQuantity(value)
+      setMintQuantity(value);
     }
-  }
+  };
 
   return (
     <div className='mint'>
@@ -91,12 +94,18 @@ function Mint({ walletConnectedDetails, connect }) {
             max={MAX_MINT}
           />
           <br />
-          <input type='submit' className='btn btn-big ' value='Soon' disabled={true} onClick={handleMintSubmit} />
+          <input
+            type='submit'
+            className='btn btn-big '
+            value='Mint'
+            disabled={false}
+            onClick={handleMintSubmit}
+          />
           {/* <input type='submit' className='btn btn-big ' value='send' onClick={onClickSend} /> */}
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default Mint
+export default Mint;
